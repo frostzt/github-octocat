cad844be349f1f2aa0d079edf273d0c8184e8f00
frostzt <aidenfrostbite@gmail.com>
2021-08-21 11:22:04 +0530

    renamed tracks and imported them in the audio player

---
+++ frontend/src/components/Pomodoro/AllSongs.js
@@ -0,0 +1,105 @@
+export const FOCUS_ALL_SONGS = [
+  {
+    id: "1",
+    type: "Focus",
+    url: `${process.env.PUBLIC_URL}/songs/F-Gyoza.mp3`,
+  },
+  {
+    id: "2",
+    type: "Focus",
+    url: `${process.env.PUBLIC_URL}/songs/F-Alone_Time.mp3`,
+  },
+  {
+    id: "3",
+    type: "Focus",
+    url: `${process.env.PUBLIC_URL}/songs/F-breeze.mp3`,
+  },
+  {
+    id: "4",
+    type: "Focus",
+    url: `${process.env.PUBLIC_URL}/songs/F-Cuddlin.mp3`,
+  },
+  {
+    id: "5",
+    type: "Focus",
+    url: `${process.env.PUBLIC_URL}/songs/F-fever.mp3`,
+  },
+  {
+    id: "6",
+    type: "Focus",
+    url: `${process.env.PUBLIC_URL}/songs/F-Lunar_Drive.mp3`,
+  },
+  {
+    id: "7",
+    type: "Focus",
+    url: `${process.env.PUBLIC_URL}/songs/F-Nova.mp3`,
+  },
+  {
+    id: "8",
+    type: "Focus",
+    url: `${process.env.PUBLIC_URL}/songs/F-Snowman.mp3`,
+  },
+  {
+    id: "9",
+    type: "Focus",
+    url: `${process.env.PUBLIC_URL}/songs/F-the_places_we_used_to_walk.mp3`,
+  },
+  {
+    id: "10",
+    type: "Focus",
+    url: `${process.env.PUBLIC_URL}/songs/F-Wish_You_Were_Mine.mp3`,
+  },
+];
+
+export const RELAX_ALL_SONGS = [
+  {
+    id: "1",
+    type: "Relax",
+    url: `${process.env.PUBLIC_URL}/songs/R-carried_away.mp3`,
+  },
+  {
+    id: "2",
+    type: "Relax",
+    url: `${process.env.PUBLIC_URL}/songs/R-Circle.mp3`,
+  },
+  {
+    id: "3",
+    type: "Relax",
+    url: `${process.env.PUBLIC_URL}/songs/R-Cotton_Cloud.mp3`,
+  },
+  {
+    id: "4",
+    type: "Relax",
+    url: `${process.env.PUBLIC_URL}/songs/R-I_m_sorry.mp3`,
+  },
+  {
+    id: "5",
+    type: "Relax",
+    url: `${process.env.PUBLIC_URL}/songs/R-Keyframe.mp3`,
+  },
+  {
+    id: "6",
+    type: "Relax",
+    url: `${process.env.PUBLIC_URL}/songs/R-Late_Night_Call.mp3`,
+  },
+  {
+    id: "7",
+    type: "Relax",
+    url: `${process.env.PUBLIC_URL}/songs/R-Owls_of_the_Night.mp3`,
+  },
+  {
+    id: "8",
+    type: "Relax",
+    url: `${process.env.PUBLIC_URL}/songs/R-Single_Phial.mp3`,
+  },
+  {
+    id: "9",
+    type: "Relax",
+    url: `${process.env.PUBLIC_URL}/songs/R-snow_sand.mp3`,
+  },
+  {
+    id: "10",
+    type: "Relax",
+    url: `${process.env.PUBLIC_URL}/songs/R-wool_gloves.mp3`,
+  },
+];
--- frontend/src/components/Pomodoro/Pomodoro.jsx
+++ frontend/src/components/Pomodoro/Pomodoro.jsx
@@ -1,14 +1,18 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, Fragment, useRef } from "react";
 
 // Components
 import Timers from "../Timers/Timers";
 import { ActivateButton } from "../Button/Button";
+import { FOCUS_ALL_SONGS, RELAX_ALL_SONGS } from "./AllSongs";
 
 // Styling
 import cx from "classnames";
 import styles from "./Pomodoro.module.scss";
 
 const Pomodoro = () => {
+  const audioRef = useRef();
+
+  // States
   const [min, setMin] = useState(0);
   const [secs, setSecs] = useState(0);
   const [focMin, setFocMin] = useState(0);
@@ -16,6 +20,11 @@ const Pomodoro = () => {
   const [start, setStart] = useState(false);
   const [isFocusing, setIsFocusing] = useState(true);
 
+  // Audio Player
+  const focusAllTracks = FOCUS_ALL_SONGS;
+  const relaxAllTracks = RELAX_ALL_SONGS;
+
+  // Handlers
   const assignMins = (to) => {
     setMin(to);
     setFocMin(to);
@@ -23,10 +32,16 @@ const Pomodoro = () => {
 
   const changeState = () => {
     if (focMin > 0 && relMin > 0) {
+      if (start) {
+        audioRef.current.pause();
+      } else {
+        audioRef.current.play();
+      }
       setStart((prevState) => !prevState);
     }
   };
 
+  // Side Effects
   useEffect(() => {
     if (start) {
       let pomodoroInterval = setInterval(() => {
@@ -57,45 +72,50 @@ const Pomodoro = () => {
   const showSecs = secs < 10 ? `0${secs}` : secs;
 
   return (
-    <div className={styles.container}>
-      {start ? null : <div className={styles.title}>Set your timer</div>}
-      <div className={cx([styles.counter, start ? styles.started : null])}>
-        {showMin}:{showSecs}
+    <Fragment>
+      <div className={styles.player}>
+        <audio ref={audioRef} src={focusAllTracks[0].url} type="audio/mp3" controls></audio>
       </div>
-      {start ? null : (
-        <div className={styles.selectors}>
-          <h3 className={styles.selectors__heading}>Focus Time</h3>
-          <div className={styles.selectors__focus}>
-            <div onClick={() => assignMins(25)}>
-              <Timers isSelected={min === 25 ? true : false} min={25} />
-            </div>
-            <div onClick={() => assignMins(30)}>
-              <Timers isSelected={min === 30 ? true : false} min={30} />
-            </div>
-            <div onClick={() => assignMins(45)}>
-              <Timers isSelected={min === 45 ? true : false} min={45} />
-            </div>
-          </div>
-          <div className={styles.selectors__relax}>
-            <h3 className={styles.selectors__heading}>Relax Time</h3>
-            <div className={styles.selectors__focus}>
-              <div onClick={() => setRelMin(5)}>
-                <Timers isSelected={relMin === 5 ? true : false} min={5} />
-              </div>
-              <div onClick={() => setRelMin(10)}>
-                <Timers isSelected={relMin === 10 ? true : false} min={10} />
-              </div>
-              <div onClick={() => setRelMin(15)}>
-                <Timers isSelected={relMin === 15 ? true : false} min={15} />
-              </div>
-            </div>
-          </div>
+      <div className={styles.container}>
+        {start ? null : <div className={styles.title}>Set your timer</div>}
+        <div className={cx([styles.counter, start ? styles.started : null])}>
+          {showMin}:{showSecs}
         </div>
-      )}
-      <ActivateButton extraClasses={styles.btn} handler={changeState}>
-        {start ? "Pause" : "Go"}
-      </ActivateButton>
-    </div>
+        {start ? null : (
+          <div className={styles.selectors}>
+            <h3 className={styles.selectors__heading}>Focus Time</h3>
+            <div className={styles.selectors__focus}>
+              <div onClick={() => assignMins(25)}>
+                <Timers isSelected={min === 25 ? true : false} min={25} />
+              </div>
+              <div onClick={() => assignMins(30)}>
+                <Timers isSelected={min === 30 ? true : false} min={30} />
+              </div>
+              <div onClick={() => assignMins(45)}>
+                <Timers isSelected={min === 45 ? true : false} min={45} />
+              </div>
+            </div>
+            <div className={styles.selectors__relax}>
+              <h3 className={styles.selectors__heading}>Relax Time</h3>
+              <div className={styles.selectors__focus}>
+                <div onClick={() => setRelMin(5)}>
+                  <Timers isSelected={relMin === 5 ? true : false} min={5} />
+                </div>
+                <div onClick={() => setRelMin(10)}>
+                  <Timers isSelected={relMin === 10 ? true : false} min={10} />
+                </div>
+                <div onClick={() => setRelMin(15)}>
+                  <Timers isSelected={relMin === 15 ? true : false} min={15} />
+                </div>
+              </div>
+            </div>
+          </div>
+        )}
+        <ActivateButton extraClasses={styles.btn} handler={changeState}>
+          {start ? "Pause" : "Go"}
+        </ActivateButton>
+      </div>
+    </Fragment>
   );
 };
 
